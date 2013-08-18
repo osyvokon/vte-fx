f19fe9f0185ead7dc1639844769c414704ca58e8
Aleksey Sivokon <aleksey.sivokon@gmail.com>
2013-08-18 04:14:39 +0300

    "Breathing" effect draft

--- cursor.lua
+++ cursor.lua
@@ -1,11 +1,34 @@
 function render (Canvas, t, x, y, w, h)
+    local stage = (t % 300);
     local k = (t % 100) / 100;
-    c = Canvas
-    local r = 0
-    local g = 1 -  k * k;
-    local b = 0
-    c:set_color(0, 0.8, 0, g)
+    local r, g, b
+    local c = Canvas
+
+    if stage < 100 then
+        r = 0
+        g = k* k ;
+        b = 0
+    elseif stage >= 100 and stage < 200 then
+        r = 0
+        g = 1
+        b = 0
+    else
+        r = 0
+        g = 1 - k * k
+        b = 0
+    end
+
+    c:set_color(1, 1, 1, 0)
     c:rectangle(x, y, w, h)
     c:fill()
+
+    c:set_color(0, 0.8, 0.3, g)
+    c:rectangle(x, y, w, h)
+    c:fill()
+
+    c:set_color(0.8, 0.8, 0.8, 0.5)
+    c:rectangle(x, y, w, h)
+    c:fill()
+
     return 30
 end
