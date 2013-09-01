77510799f4c7ce204be6f786728213aea7d185a2
Aleksey Sivokon <aleksey.sivokon@gmail.com>
2013-09-01 10:26:03 +0300

    Updated cursor

--- cursor-grad.lua
+++ cursor-grad.lua
@@ -1,13 +1,33 @@
 -- moving gradient
 function render (c, t, x, y, w, h)
-    local k = (t % 100) / 100, k2;
+--    local rainbow = {
+        --{1,  0,   0},
+        --{1,  0.5, 0},
+        --{1,  1,   0},
+        --{1,  1,   0},
+        --{0,  1,   0},
+        --{0,  0,   1},
+        --{0.33, 0, 0.5},
+        --{0.5, 0,  1}}
 
-    for j = 1, h do
-        k2 = 1 - ((j + t) % h) / h;
+    c:linear_grad(x, y, x, y+h);
+    c:add_color_stop(0,
+                     math.sin(t / 15 + 0) ,
+                     math.sin(t / 15 + 2) ,
+                     math.sin(t / 15 + 4) ,
+                     0.8)
+    --c:add_color_stop(0.5, c1[1], c1[2], c1[3], 0.8)
+    c:add_color_stop(1,
+                     math.sin(t / 15 + 0.2) ,
+                     math.sin(t / 15 + 2.2) ,
+                     math.sin(t / 15 + 4.2) ,
+                     0.8)
+    --c:add_color_stop(0.5, c1[1], c1[2], c1[3], 0.8)
+    --c:add_color_stop(0, c2[1], c2[2], c2[3], 0.8)
+    --c:add_color_stop(0.8 , math.sin(t / 10), math.sin(t / 50), 0, 0.5)
+    c:rectangle(x, y, w, h)
+    c:fill()
 
-        c:set_color(0.2, 0.2, 1, k2)
-        c:rectangle(x, y + j, w, 2)
-        c:fill()
-    end
-    return 80
+    return 100
 end
+
